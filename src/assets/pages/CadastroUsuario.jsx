110739e2b2cd067013ef19d7ce77b9aadf5ada6b
Emelly Freitas <emedoim333@gmail.com>
2024-11-11 10:04:29 -0300

    integração com login

--- src/assets/pages/CadastroUsuario.jsx
+++ src/assets/pages/CadastroUsuario.jsx
@@ -3,21 +3,54 @@ import { Link } from "react-router-dom";
 import axiosInstance from "../services/api";
 
 function CadastroUsuario() {
+
+    const [nome, setNome] = useState('');
+    const [email, setEmail] = useState('');
+    const [senha, setSenha] = useState('')
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await axiosInstance.post('/users', {
+                nome_completo: nome,
+                email: email,
+                senha: senha
+            })
+            console.log(response);
+            setNome("");
+            setEmail("");
+            setSenha("");
+        }
+        catch (error) {
+            console.log("Erro ao cadastrar o usuário: ", error);
+        }
+    }
+
     return (
         <div className="w-screen h-screen flex items-center justify-center bg-[#DEE0E0]">
-            <form className="bg-white rounded-xl p-6 md:p-10 flex flex-col justify-center items-center max-w-md w-full shadow-xl m-5">
+            <form className="bg-white rounded-xl p-6 md:p-10 flex flex-col justify-center items-center max-w-md w-full shadow-xl m-5" onSubmit={handleSubmit}>
                 <h1 className="font-bold text-green-600 text-2xl mb-5">RecicleJá</h1>
                 <div class="mb-5 w-full">
                     <label for="name" class="block mb-2 text-sm ">Nome Completo</label>
-                    <input type="text" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"  required />
+                    <input type="text" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                        value={nome}
+                        onChange={(e) => setNome(e.target.value)}
+                        required
+                    />
                 </div>
                 <div class="mb-5 w-full">
                     <label for="email" class="block mb-2 text-sm ">E-mail</label>
-                    <input type="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@gmail.com" required />
+                    <input type="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@gmail.com"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        required />
                 </div>
                 <div class="mb-5 w-full">
                     <label for="password" class="block mb-2 text-sm ">Senha</label>
-                    <input type="password" id="password" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" required />
+                    <input type="password" id="password" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                        value={senha}
+                        onChange={(e) => setSenha(e.target.value)}
+                        required />
                 </div>
                 <button
                     type="subimit"
