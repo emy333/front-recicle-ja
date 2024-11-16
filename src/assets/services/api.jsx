ac7ff0311b241df6e75a1ebd660749aa57bef0e4
Emelly Freitas <emy333>
2024-11-16 00:25:42 -0300

    ajuste url api

--- src/assets/services/api.jsx
+++ src/assets/services/api.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
     //baseURL: "http://localhost:8080",
-    baseURL: "http://localhost:8080",
+    baseURL: "https://recile-1qhjo2wt.b4a.run",
 });
 
 export default axiosInstance;
